3d7cdc1955656b3200355cf12d8e8b76da9231aa
root <joshlevy89@gmail.com>
2024-02-26 23:19:05 +0000

    Got code working on vast

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-cd /notebooks/got-replication
 pip install plotly
-pip uninstall torch torchvision torchaudio -y
-pip3 install transformers>=4.32.0 optimum>=1.12.0
+pip install transformers>=4.32.0
 pip install torch==2.1.1 torchvision==0.16.1 torchaudio==2.1.1 --index-url https://download.pytorch.org/whl/cu121
+pip install optimum>=1.12.0
 pip install auto-gptq==0.6.0
 pip install tokenizers==0.13.3
 pip install --upgrade transformers accelerate
 pip install nbdime
 nbdime config-git --enable --global
+pip install scikit-learn
+pip install matplotlib
